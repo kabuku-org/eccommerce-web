053d5c200defc5d8ca5ed644ba7328a9b9c91002
emphemeral-tech <githaigakabuku@gmail.com>
2026-06-25 22:32:55 +0300

    added shaping prduct quries and mutation use gql

---
+++ src/apollo/queries/product.queries.ts
@@ -0,0 +1,84 @@
+import {gql} from '@apollo/client'
+import type { Product } from '../../types/product.types'
+
+export const GET_PRODUCTS = gql`
+  query GetProducts {
+    products {
+      id
+      name
+      description
+      price
+      stock
+      image_url
+      created_at
+    }
+  }
+`
+
+export const GET_PRODUCT = gql`
+  query GetProduct($id: String!) {
+    product(id: $id) {
+      id
+      name
+      description
+      price
+      stock
+      image_url
+      created_at
+    }
+  }
+`
+
+export const CREATE_PRODUCT = gql`
+  mutation CreateProduct($input: CreateProductInput!) {
+    createProduct(input: $input) {
+      id
+      name
+      description
+      price
+      stock
+      image_url
+      created_at
+    }
+  }
+`
+
+export const UPDATE_PRODUCT = gql`
+  mutation UpdateProduct($id: String!, $input: UpdateProductInput!) {
+    updateProduct(id: $id, input: $input) {
+      id
+      name
+      description
+      price
+      stock
+      image_url
+      created_at
+    }
+  }
+`
+
+export const DELETE_PRODUCT = gql`
+  mutation DeleteProduct($id: String!) {
+    deleteProduct(id: $id)
+  }
+`
+
+export type GetProductsResponse = {
+  products: Product[]
+}
+
+export type GetProductResponse = {
+  product: Product
+}
+
+export type CreateProductResponse = {
+  createProduct: Product
+}
+
+export type UpdateProductResponse = {
+  updateProduct: Product
+}
+
+export type DeleteProductResponse = {
+  deleteProduct: boolean
+}
