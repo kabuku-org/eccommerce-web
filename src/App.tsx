870b695138c54380f16ef47dfb3cdbfddef8de2b
emphemeral-tech <githaigakabuku@gmail.com>
2026-06-25 22:22:34 +0300

    addded protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './pages/Auth/Login'
 import { Signup } from './pages/Auth/Signup'
-
+import ProtectedRoutes from './pages/Auth/ProtectedRoutes'
 function Home() {
   return <div className="p-8">Home page — books go here next</div>
 }
@@ -13,6 +13,13 @@ export function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+       
+       {/* Add protected routes here */}
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/dashboard" element={<div>Dashboard - Protected</div>} />
+          {/* Add more protected routes as needed */}
+        </Route>
+
       </Routes>
     </BrowserRouter>
   )
